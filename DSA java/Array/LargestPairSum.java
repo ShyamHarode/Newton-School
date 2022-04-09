e193918160e24d9939a5b4c5b70338a52a041d7f
SHYAM HARODE <100039836+ShyamHarode@users.noreply.github.com>
2022-04-09 16:23:37 +0530

    Create LargestPairSum.java

---
+++ DSA java/Array/LargestPairSum.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+       int arr [] = {10, 12, -5, 15, -6};
+        int n = arr.length;
+        Arrays.sort(arr);
+        int sum = 0;
+        int large1 = Integer.MIN_VALUE;
+        int large2 = Integer.MIN_VALUE;
+        for(int i =0; i<n; i++){
+            if(arr[i]>large1){
+                large2=large1;
+                large1=arr[i];
+            }
+            else if(arr[i]>large2){
+                large2 = arr[i];
+            }
+        }
+        System.out.println(large1);
+        System.out.println(large2);
+        System.out.print(large1 + large2);
+    }
+}
+// O(n) time
